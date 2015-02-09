e03f763b2d89f66a5c4452c8eeab162684920d53
Andreas Schäfer <gentryx@gmx.de>
2015-02-09 07:31:31 +0100

    extracted some methods to clarify code

--- fortran_parser.rb
+++ fortran_parser.rb
@@ -39,93 +39,14 @@ class FortranParser
     stack = [tree]
 
     File.open(infile) do |infile|
-      aggregated_lines = []
-      raw_lines = []
+      lines = separate_comments(infile.readlines)
+      lines = aggregate_lines(lines)
+      lines = prune_dead_preprocessor_branches(lines)
 
-      line_counter = 0
-      # we need to strip comments first to correctly handle line continuations later on
-      infile.readlines.each do |line|
-        line_counter += 1
-        new_line = {
-          :number => line_counter,
-          :end => line_counter,
-          :cargo => "",
-          :comments => []
-        }
-
-        case
-        when line =~ /^c(.*)/i
-          # we need to strip pure comment lines and append those to the
-          # preceeding line to ensure continuation works correctly
-          if raw_lines.size > 0
-            # we can only append if there actually is a preceeding line
-            raw_lines[-1][:comments] << $1
-            next
-          end
-          new_line[:comments] << $1
-        when line =~ /^([^\!]*)\!(.*)$/
-          new_line[:cargo] += $1
-          new_line[:comments] << $2
-        when line =~ /^$/
-          # drop empty lines, also required to correctly fuse continued
-          # lines (continuations ignore interleaved empty lines).
-          next
-        else
-          new_line[:cargo] += line.chomp
-        end
-
-        raw_lines << new_line
-      end
-
-      # handle line continuation/aggregation
-      raw_lines.each do |line|
-        if line[:cargo] =~ /^\s+\&\s*(.*)$/
-          raise "no preceeding line to fuse with" if aggregated_lines.size == 0
-          aggregated_lines[-1][:cargo] += " " + $1
-          aggregated_lines[-1][:comments] += line[:comments]
-          aggregated_lines[-1][:end] = line[:number]
-          next
-        end
-
-        if (aggregated_lines.size > 0) && (aggregated_lines[-1][:cargo] =~ /^(.*)\&\s*$/)
-          aggregated_lines[-1][:cargo] = $1.strip + " " + line[:cargo].strip
-          aggregated_lines[-1][:comments] += line[:comments]
-          aggregated_lines[-1][:end] = line[:number]
-          next
-        end
-
-        aggregated_lines << line
-      end
-
-      # prune "#if 1" clauses. if we were doing this right, we'd need two
-      # parser passes (first for c-preprocessor, second for Fortran), but
-      # we really want to preserve the preprocessor statements, so we're
-      # just cutting away the most superfluous stuff.
-      cleared_lines = []
-      state = :discharged
-      aggregated_lines.each do |line|
-        if line[:cargo] =~ /^#if 1/
-          state = :charged
-        end
-
-        if line[:cargo] =~ /^#endif/
-          state = :discharged
-        end
-
-        if (line[:cargo] =~/^#else/) && (state == :charged)
-          state = :ablaze
-        end
-
-        if state != :ablaze
-          cleared_lines << line
-        end
-      end
-
-      cleared_lines.each do |line|
+      lines.each do |line|
         # fixme: we should also keep the line end number (in case we've aggregated some lines)
         parse_line(line[:number], stack, line[:cargo], line[:comments])
       end
-
     end
 
     return tree
@@ -170,6 +91,101 @@ class FortranParser
     return if AssignmentNode.accept(   *args)
 
     raise "encounted unknown node in line #{line_counter}: »#{line}«"
+  end
+
+  # we need to strip comments first to correctly handle line continuations later on
+  def separate_comments(lines)
+    line_counter = 0
+    raw_lines = []
+
+    lines.each do |line|
+      line_counter += 1
+      new_line = {
+        :number => line_counter,
+        :end => line_counter,
+        :cargo => "",
+        :comments => []
+      }
+
+      case
+      when line =~ /^c(.*)/i
+        # we need to strip pure comment lines and append those to the
+        # preceeding line to ensure continuation works correctly
+        if raw_lines.size > 0
+          # we can only append if there actually is a preceeding line
+          raw_lines[-1][:comments] << $1
+          next
+        end
+        new_line[:comments] << $1
+      when line =~ /^([^\!]*)\!(.*)$/
+        new_line[:cargo] += $1
+        new_line[:comments] << $2
+      when line =~ /^$/
+        # drop empty lines, also required to correctly fuse continued
+        # lines (continuations ignore interleaved empty lines).
+        next
+      else
+        new_line[:cargo] += line.chomp
+      end
+
+      raw_lines << new_line
+    end
+
+    return raw_lines
+  end
+
+  # handle line continuation/aggregation
+  def aggregate_lines(raw_lines)
+    aggregated_lines = []
+
+    raw_lines.each do |line|
+      if line[:cargo] =~ /^\s+\&\s*(.*)$/
+        raise "no preceeding line to fuse with" if aggregated_lines.size == 0
+        aggregated_lines[-1][:cargo] += " " + $1
+        aggregated_lines[-1][:comments] += line[:comments]
+        aggregated_lines[-1][:end] = line[:number]
+        next
+      end
+
+      if (aggregated_lines.size > 0) && (aggregated_lines[-1][:cargo] =~ /^(.*)\&\s*$/)
+        aggregated_lines[-1][:cargo] = $1.strip + " " + line[:cargo].strip
+        aggregated_lines[-1][:comments] += line[:comments]
+        aggregated_lines[-1][:end] = line[:number]
+        next
+      end
+
+      aggregated_lines << line
+    end
+
+    return aggregated_lines
+  end
+
+  # prune "#if 1" clauses. if we were doing this right, we'd need two
+  # parser passes (first for c-preprocessor, second for Fortran), but
+  # we really want to preserve the preprocessor statements, so we're
+  # just cutting away the most superfluous stuff.
+  def prune_dead_preprocessor_branches(aggregated_lines)
+    cleared_lines = []
+    state = :discharged
+    aggregated_lines.each do |line|
+      if line[:cargo] =~ /^#if 1/
+        state = :charged
+      end
+
+      if line[:cargo] =~ /^#endif/
+        state = :discharged
+      end
+
+      if (line[:cargo] =~/^#else/) && (state == :charged)
+        state = :ablaze
+      end
+
+      if state != :ablaze
+        cleared_lines << line
+      end
+    end
+
+    return cleared_lines
   end
 
 end
