456fa7b6b875691890b37c434b57da9603436100
Andreas Schäfer <gentryx@gmx.de>
2015-02-09 07:29:46 +0100

    chop whitespace from definition nodes

--- definition_node.rb
+++ definition_node.rb
@@ -2,8 +2,8 @@ load './syntax_node.rb'
 
 class DefinitionNode < SyntaxNode
   def self.accept(line, stack, line_counter, new_indentation, comments)
-    if line =~ /^\s*(character|complex|integer|real|logical)(\(\w+\))?,?\s+(.+)/i
-      new_node = DefinitionNode.new(line_counter, :definition, new_indentation, line.chomp, comments)
+    if line =~ /^\s*((character|complex|integer|real|logical)(\(\w+\))?,?\s+(.+))/i
+      new_node = DefinitionNode.new(line_counter, :definition, new_indentation, $1.chomp, comments)
       stack.last << new_node
       return true
     end
