462150e020e7f3ffe40079eea8df6cc22ef228dc
Andreas Schäfer <gentryx@gmx.de>
2015-02-09 09:55:55 +0100

    definition expression is now much more targeted

--- definition_node.rb
+++ definition_node.rb
@@ -2,8 +2,10 @@ load './syntax_node.rb'
 
 class DefinitionNode < SyntaxNode
   def self.accept(line, stack, line_counter, new_indentation, comments)
-    if line =~ /^\s*((character|complex|integer|real|logical)(\(\w+(=\w+)?\))?,?\s+(.+))/i
-      new_node = DefinitionNode.new(line_counter, :definition, new_indentation, $1.chomp, comments)
+    if line =~ /^\s*(((character|complex|integer|real|logical)(\([^\)\n]+\))?)(,\s*\w+(\([^\)\n]+\))?)*)\s*(::)?\s+(\w+(\([^\)\n]*\))?(\s*,\s*\w+(\([^\)\n]+\))?)*)\s*$/i
+
+      cargo = $1.chomp + " " + $8.chomp
+      new_node = DefinitionNode.new(line_counter, :definition, new_indentation, cargo, comments)
       stack.last << new_node
       return true
     end
