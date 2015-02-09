63ea59aed1bd9c3ab89f9c832a4cf6878cd43c1e
Andreas Schäfer <gentryx@gmx.de>
2015-02-09 07:30:41 +0100

    tailor matched cargo for subroutine node

--- subroutine_node.rb
+++ subroutine_node.rb
@@ -2,8 +2,8 @@ load './syntax_node.rb'
 
 class SubroutineNode < SyntaxNode
   def self.accept(line, stack, line_counter, new_indentation, comments)
-    if line =~ /^\s*subroutine (\w+)\s*\((.*)\)\s*$/i
-      new_node = SubroutineNode.new(line_counter, :subroutine, new_indentation, line.chomp, comments)
+    if line =~ /^\s*subroutine ((\w+)\s*\((.*)\))\s*$/i
+      new_node = SubroutineNode.new(line_counter, :subroutine, new_indentation, $1.chomp, comments)
       stack.last << new_node
       stack << new_node
       return true
