cf52278209ecfe84f81e712b8473cf8cf22c0449
Andreas Schäfer <gentryx@gmx.de>
2015-02-11 07:42:37 +0100

    relaxed write() matching

--- write_node.rb
+++ write_node.rb
@@ -2,7 +2,7 @@ load './syntax_node.rb'
 
 class WriteNode < SyntaxNode
   def self.accept(line, stack, line_counter, new_indentation, comments)
-    if line =~ /^\s*write\s*\(.*\)\s+.+$/i
+    if line =~ /^\s*write\s*\(.*\)\s*.*$/i
       new_node = WriteNode.new(line_counter, :write, new_indentation, line.chomp, comments)
       stack.last << new_node
       return true
