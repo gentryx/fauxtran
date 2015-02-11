1bbea7580dd80fa5772d6d76669bfd892dbbdcb3
Andreas Schäfer <gentryx@gmx.de>
2015-02-11 08:16:37 +0100

    consistent naming

--- preprocessor_node.rb
+++ preprocessor_node.rb
@@ -3,7 +3,7 @@ load './syntax_node.rb'
 class PreprocessorNode < SyntaxNode
   def self.accept(line, stack, line_counter, new_indentation, comments)
     if line =~ /^#/
-      new_node = PreprocessorNode.new(line_counter, :preproc,    new_indentation, line.chomp, comments)
+      new_node = PreprocessorNode.new(line_counter, :preprocessor, new_indentation, line.chomp, comments)
       stack.last << new_node
       return true
     end
